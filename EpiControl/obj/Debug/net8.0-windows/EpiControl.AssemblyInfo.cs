18fcfc6a424a89bb13abae6fd82d21762c47f885
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2025-12-28 22:35:09 -0300

    🔧 chore(dependencias): adicionar dependencias do projeto

--- EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
+++ EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e686d1c468dd785b99a92e8aa75ce8a1e3772db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0733405cdc7d06dc05ee30a45dd2dfe614abe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
