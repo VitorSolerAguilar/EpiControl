bd0733405cdc7d06dc05ee30a45dd2dfe614abe7
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2025-12-28 22:26:24 -0300

    ♻️ refactor(estrutura): padronizar nomes das pastas do projeto

--- EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
+++ EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb55407a9daf66d5655a0a74bf164d40bc781edd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e686d1c468dd785b99a92e8aa75ce8a1e3772db")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
