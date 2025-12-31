576c47b7bb5c88cd87139f0ee96680353bacde61
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2025-12-31 03:38:25 -0300

    ✨ feat(Funcionario): implementar camada model

---
+++ EpiControl/epicontrol.model/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpiControl.epicontrol.model
+{
+	public class Funcionario
+	{
+		public int Id { get; set; }
+		public string nome { get; set; }
+		public string rg { get; set; }
+		public string cpf { get; set; }
+		public string pis { get; set; }
+		public DateTime dataNascimento { get; set; }
+		public string numeroCTPS { get; set; }
+		public string serieCtps { get; set; }
+		public string estadoCivil { get; set; }
+		public string genero { get; set; }
+		public string nomeMae { get; set; }
+		public string nomePai { get; set; }
+		public string inscricaoETitulo { get; set; }
+		public string zona { get; set; }
+		public string secao { get; set; }
+		public string municipio { get; set; }
+		public string uf { get; set; }
+		public DateTime dataEmissao { get; set; }
+	}
+}
