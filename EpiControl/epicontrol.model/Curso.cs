77bf548ad4fda9db47f864d3515a5461049bc539
KatiaCursi <cursikatia@gmail.com>
2026-03-19 22:27:15 -0300

    ✨ feat(cursoModel): Model do Curso

--- EpiControl/epicontrol.model/Curso.cs
+++ EpiControl/epicontrol.model/Curso.cs
@@ -8,5 +8,11 @@ namespace EpiControl.epicontrol.model
 {
     internal class Curso
     {
+        public int IdCurso { get; set; }
+        public string Nome { get; set; }
+        public string CargaHoraria { get; set; }
+        public string Descricao { get; set; }
+        public int ValidadeMeses { get; set; }
+        public string TbCursocol { get; set; }
     }
 }
