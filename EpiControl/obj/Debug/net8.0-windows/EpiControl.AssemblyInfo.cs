0ac12861b9523475ed93f4830adbd245a256f0d7
KatiaCursi <cursikatia@gmail.com>
2026-03-19 21:41:06 -0300

    🐛 fix(telaFuncionarioCurso): ajuste na tela FuncionarioCurso

--- EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
+++ EpiControl/obj/Debug/net8.0-windows/EpiControl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec2ff65b25d7831328cb31e93f46ebae7308cf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7dda5640d3fccfaeb27fef4a8761fff24b6dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EpiControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
